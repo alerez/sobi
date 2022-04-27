bfecc7e123b090f579d4b7afcf169a286192092d
Олександр Резніченко <alex.reznichenko2001@gmail.com>
2022-04-27 17:21:50 +0300

    init

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -12,7 +12,7 @@
         <div class="main__download download">
           <div class="download__body">
             <div class="download__text">{{ store.mainData.text }}</div>
-            <div class="download__button" @click="store.showModal = true">{{ store.mainData.button }}</div>
+            <div class="download__button" @click="store.showModal = true" >{{ store.mainData.button }}</div>
           </div>
         </div>
       </div>
